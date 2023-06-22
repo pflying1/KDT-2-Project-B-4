6ace2da5c7a1a127e1e67bb63ae669d9fd7c8779
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-06-22 11:46:05 +0900

    라우터 만들고 저장

---
+++ develop/src/Main.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ComponentPrac from "./component-prac";
+
+// TS에서는 React.ReactNode를 배정하면 이해하지 못한다. 그렇기에 공식 홈페이지에서 이를 해석하기 위해 JSX 타입을 사용한다.
+// JSX 타입의 경우 함수 형식(JSX.Element)과 클래스 형식(JSX.ElementClass)으로 나뉜다.
+type FCReactNode = JSX.Element;
+
+// 함수형 컴포넌트
+function App(): FCReactNode {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ComponentPrac />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
+
+const rootElement = document.getElementById("root") as HTMLElement;
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(<App />);
+} else {
+  console.error("Root element not found");
+}
---
+++ develop/src/component-prac.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const componentPrac = () => {
+  return (
+    <div>
+      <div>시작</div>
+    </div>
+  )
+}
+export default componentPrac

2bdaaaaf7fbddcd7245217e4cb00d6870bf1c8be
KimEunA97 <kimeuna9712@gmail.com>
2023-06-22 11:24:55 +0900

    typescript 만 번들링 되도록 설정된 webpack config 파일

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,24 @@
 //수동 생성
+const path = require('path');
+
+module.exports = {
+  entry: './build/src/index.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+
+  resolve: {
+    extension: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  }
+};

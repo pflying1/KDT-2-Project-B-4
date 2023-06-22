b94745755b77d30135b31fd65a00c5e4a2cb1d6e
gwonyejun <kyjuni1059@gmail.com>
2023-06-22 15:37:05 +0900

    완성

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'), // public 디렉토리를 정적 파일 제공 디렉토리로 설정
+    open: true,
+    hot: true, // HMR 활성화
+  },
+};

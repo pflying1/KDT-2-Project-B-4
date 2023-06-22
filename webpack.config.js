351c4c6d368ec82b6ea9ea63d2f428000ee818b4
gwonyejun <kyjuni1059@gmail.com>
2023-06-22 11:24:14 +0900

    각자 번들링 하여 hello world 띄우기 Fixes #1

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './test.ts',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.ts$/, /\.tsx$/],
+        use: ['babel-loader', 'ts-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', ',tsx'],
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    port: 8080,
+  },
+};

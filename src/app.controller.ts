dfb3dd094082fdff9928c559f7336a4354a40249
gwonyejun <kyjuni1059@gmail.com>
2023-06-23 16:17:45 +0900

    번들링된 파일 화면 띄우기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Res ,Req} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('*')
+  serveFile(@Req() req: Request, @Res() res: Response) {
+    const filePath = join(__dirname, '..', 'client', 'build','index.html');
+    return res.sendFile(filePath);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // 정적 파일 제공 설정
+  app.useStaticAssets(join(__dirname, '..','client', 'build'));
+
+
   await app.listen(3000);
 }
 bootstrap();

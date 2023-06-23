ddc3960764a250388901dad4e7b54cc9fa1f2991
jeongseongcheol <tjdcjf9333@gmail.com>
2023-06-23 20:07:35 +0900

    데이터를 실시간으로 계속 받아오는 문제 있어서 useEffect로 한번만 실행하도록 바꿈

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [hello, setHello] = useState()
-  fetch('http://localhost:3030/test') // 백엔드 Nest.js 서버의 주소와 포트 번호를 입력하십시오
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    // const helloHi = JSON.stringify(data)
-    // 받아온 데이터로 필요한 작업 수행
-    setHello(data)
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3030/test');
+        const data = await response.json();
+        setHello(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div>
       <div>{hello}</div>

4ad285df0df0859cadf1bad590b88148985f1708
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-06-23 13:07:40 +0900

    react 파일 설정

---
+++ develop/view/AboutPage.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import Component from "./components/Component-prac"
+
+function AboutPage() : JSX.Element {
+  return(
+    <div>
+      <Component />
+    </div>
+  )
+}
+export default AboutPage
---
+++ develop/view/Router.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Link } from "react-router-dom"
+
+
+const Router = () => {
+  return (
+    <div>
+      <Link to="">
+        <button type="button">메인페이지</button>
+      </Link>
+    </div>
+  )
+}
+export default Router

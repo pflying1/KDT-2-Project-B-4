f4daa5db176fb702ebd9fa707d20c9e13b9b07e2
gwonyejun <kyjuni1059@gmail.com>
2023-06-25 17:18:12 +0900

    몽고db import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, MongooseModule.forRoot('mongodb+srv://adminID:adminPW@project4.uk45ojb.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })

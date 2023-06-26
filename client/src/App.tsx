eabf8d763f6acaf5ec863c3cb8ab11efd17aa958
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-06-26 18:08:27 +0900

    즐겨찾기 리스트 컴포넌트 하나 작성중

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import CreateFrom from './createFrom'
+import FavoritesList from './components/FavoritesList'
 function App() {
   return (
     <div className="App">
-      <CreateFrom />
+      {/* <CreateFrom /> */}
+      <FavoritesList />
     </div>
   );
 }
---
+++ client/src/components/FavoritesList.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import bookmark from "./images/bookmark.png"
+
+const FavoritesList = () => {
+  return(
+    <div>
+      <div>
+        <div>을지대학병원</div>
+        <img src="bookmark"></img>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+export default FavoritesList

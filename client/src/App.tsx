69fc849b9dbb4c75d38ebb26ea8b1d5377730d97
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-06-27 11:59:43 +0900

    style을 css파일로 분리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import './component/view/css/SearchBarStyle.css'
 import CreateFrom from './component/view/createFrom'
-import SearchBar from './SearchBar';
+import SearchBar from './component/view/SearchBar';
 function App() {
   return (
     <div className="App">

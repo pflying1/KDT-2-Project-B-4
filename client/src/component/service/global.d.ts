2c5537c1b29fefa064a79b81f831909d5eb18d3a
gwonyejun <kyjuni1059@gmail.com>
2023-06-27 23:17:16 +0900

    카카오 지도 api 띄우기 Fixes #7

---
+++ client/src/component/service/global.d.ts
@@ -0,0 +1,27 @@
+// src/types/kakao.maps.d.ts
+declare namespace kakao.maps {
+  // 여기에 카카오 맵의 여러 가지 타입 정의를 추가...
+  // 예시:
+  class Map {
+    constructor(container: HTMLElement, options: object);
+  }
+
+  class LatLng {
+    constructor(lat: number, lng: number);
+  }
+
+  function load(callback: () => void): void;
+}
+
+declare global {
+  interface Window {
+    kakao: any; 
+  }
+}
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+export {};

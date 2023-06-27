f57dc242815e98fe0650c4dad79f23f8983a5002
gwonyejun <kyjuni1059@gmail.com>
2023-06-27 14:52:47 +0900

    인트로 화면 만들었습니다 경로 / 들어가게 되면 인트로 화면 보입니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import './App.css';
-import CreateFrom from './component/view/createFrom'
 import Map from './component/service/kakaoMap'
 import MenuBar from './component/view/menuBar'
-
+import IntroPage from "./component/view/introPageScreen";
+import Nav from './component/view/nav';
+import Header from "./component/view/header";
 function App() {
+  const location = useLocation();
+  const [pageTitle, setPageTitle] = useState("");
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/":
+        setPageTitle("");
+        break;
+      case "/main":
+        setPageTitle("홈");
+        break;
+      case "/bookMake":
+        setPageTitle("내 정보");
+        break;
+      default:
+        setPageTitle("홈");
+        break;
+    }
+  }, [location.pathname]);
   return (
-    <div className="App">
-      {/* <CreateFrom /> */}
-      <div className='container'>
-        <div className='containerHead'>
 
-        </div>
-        <div className='containerBody'>
-          <Map />
-        </div>
-        <div className='containerFoot'>
-          <MenuBar />
-        </div>
+    <div className="App">
+
+      <div className='container'>
+        <Header />
+        <Routes>
+          <Route path="/" element={<IntroPage />} />
+          <Route path="/map" element={<Map />} />
+        </Routes>
+        {[
+          "/"
+        ].includes(location.pathname) ? null : (
+          <Nav />
+        )}
+    
       </div>
     </div>
-    
+
   );
 }
 
 export default App;
+
+
---
+++ client/src/component/service/introPage.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import introImg from '../view/image/introImg.png';
+type LoadingProps = {};
+
+// 함수형 컴포넌트의 이름을 "IntroPage"로 수정합니다. (대문자 시작)
+function IntroPage(props: LoadingProps) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/main');
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <img src={introImg} width='100%' height='100%' alt='이미지' />
+    </>
+  );
+}
+
+export default IntroPage; // 수정된 컴포넌트 이름으로 변경합니다.

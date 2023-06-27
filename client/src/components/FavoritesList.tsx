fc42f6b9eef9342aeff5c243c6aefa970af5c649
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-06-27 10:49:58 +0900

    즐겨찾기 컴포넌트 하나 작성중 저장

--- client/src/components/FavoritesList.tsx
+++ client/src/components/FavoritesList.tsx
@@ -1,15 +1,30 @@
 import React from "react";
-import bookmark from "./images/bookmark.png"
+import BookmarkImg from "../images/bookmark.png"
 
 const FavoritesList = () => {
+  const titleAndImgCss = {
+    display : "flex",
+    justifyContent: "space-evenly"
+  }
+  const cityHallCss = {
+    color: "gray"
+  }
   return(
     <div>
-      <div>
-        <div>을지대학병원</div>
-        <img src="bookmark"></img>
+      <div style={titleAndImgCss}>
+        <h3>을지대학병원</h3>
+        <div style={cityHallCss}>대전 시청 방면</div>
+        <img src={BookmarkImg} alt=""></img>
       </div>
       <div>
-        <div></div>
+        <div>
+          <div>
+            <div style={{width : "40px", color : "white", background : "#00900"}}>지선</div>
+            <div></div>
+          </div>  
+          <div></div>
+        </div>
+
         <div></div>
         <div></div>
       </div>

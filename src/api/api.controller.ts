5e1e9e41c1b5fceafac3cb58fb714c2cdb131cce
gwonyejun <kyjuni1059@gmail.com>
2023-06-28 15:23:07 +0900

    env방식 중 configService 설정

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get, Res } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config'; // ConfigService를 import
 import { Response } from 'express';
+import 'dotenv/config';
+import { ConfigService } from '@nestjs/config'; // 
 @Controller('api')
 export class ApiController {
+  constructor(private configService: ConfigService) {} // ConfigService를 주입
+
   @Get('apiKey')
   getApiKey(@Res() res: Response) {
-    const apiKey = process.env.KAKAO_KEY; // apiKey 값을 서버에서 가져오는 로직을 구현해야 합니다.
-    console.log('apikey',apiKey)
+    const apiKey = this.configService.get('KAKAO_KEY'); // 환경 변수의 값을 가져옵니다.
+    console.log(apiKey);
     // apiKey 값을 클라이언트로 응답합니다.
-    return res.json(apiKey);
+    return res.json({ apiKey });
   }
 }

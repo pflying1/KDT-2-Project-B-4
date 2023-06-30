31c5c85648227af4a4b0bbe4ce80a50d62cddd3e
KimEunA97 <kimeuna9712@gmail.com>
2023-07-01 00:47:42 +0900

    '예'만 입력해도 이벤트 발생

--- client/src/component/view/SearchBar.tsx
+++ client/src/component/view/SearchBar.tsx
@@ -1,36 +1,51 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 // import ReactDOM from 'react-dom';
 import menuBox from './image/menu.png';
 import searchBox from './image/search.png';
 import "./css/SearchBarStyle.css";
 import axios from 'axios'
 
-const handleSearch = async (): Promise<void> => {
-  const inputValue: string = (document.querySelector('.searchInputCss') as HTMLInputElement).value;
-
-  try {
-    const response = await axios.get('http://localhost:3000/search/', {
-      params: {
-        input: inputValue,
-      },
-    });
-    const data = response.data;
-    // 일치하는 값을 처리하거나 상태를 업데이트합니다.
-  } catch (error) {
-    // 에러 처리
-  }
-};
 
 const SearchBar = () => {
+  const [message, setMessage] = useState<string>('');
+  const [inputValue, setInputValue] = useState('');
+  const [result, setResult] = useState('');
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+
+    if (obj.name === value) {
+      setInputValue(value);
+      setResult(obj.age);
+    } else {
+      setInputValue(value);
+      setResult('');
+    }
+  };
+
+  const obj = {
+    name: '예준',
+    age: '25',
+  };
+
+
+
+  console.log(onChange);
   return (
+
     <div className="mainCss">
       <div className="searchContainerCss">
         <img src={menuBox} alt='menu' className="menuCss" />
-        <input className="searchInputCss" placeholder='정류소 검색' />
-        <button className="searchContainerTextCss" onClick={handleSearch} type='submit'>
+        <input className="searchInputCss" placeholder='정류소 검색' onChange={onChange} type='text' value={inputValue} />
+
+        <button className="searchContainerTextCss" >
           <div>검색</div>
           <img src={searchBox} alt="search" />
         </button>
+      </div>
+      <div>
+        <p>현재 입력값: {inputValue}</p>
+        <p>결과: {result}</p>
       </div>
     </div>
   );

b7d65f9ee2f01f14b91a6aa57482ac5f67a8a4ed
euni.ryu <euni.ryu97@gmail.com>
2023-07-05 16:24:26 +0900

    모달창 그냥 5초뒤에 꺼지도록함

--- client/src/component/service/kakaoMap.tsx
+++ client/src/component/service/kakaoMap.tsx
@@ -35,7 +35,7 @@ let busTime = '6'
 let busStopCount = '4'
 let toggle = false;
 let mark = '';
-
+let ID;
 
 let responseHandlerRegistered = false;
 const MapWithMarkers: React.FC<MapProps> = () => {
@@ -81,44 +81,23 @@ const MapWithMarkers: React.FC<MapProps> = () => {
             .then((response) => response.json())
             .then((data) => {
               
-              let position:any;
-              let marker:any;
-              let gpsLati 
-              let gpsLong 
-              let gpsBusName
-              let gpsBusNodeId :any
-              let aaaMarker;
 
               data.forEach((item: BusStopData) => {
-                gpsLati = item.GPS_LATI[0];
-                gpsLong = item.GPS_LONG[0];
-                gpsBusName = item.BUSSTOP_NM[0];
-                gpsBusNodeId = item.BUS_NODE_ID[0];
+                let gpsLati = item.GPS_LATI[0];
+                let gpsLong = item.GPS_LONG[0];
+                let gpsBusName = item.BUSSTOP_NM[0];
+                let gpsBusNodeId = item.BUS_NODE_ID[0];
 
                 // 마커를 표시할 위치입니다
-                position = new window.kakao.maps.LatLng(gpsLati, gpsLong);
+                let position = new window.kakao.maps.LatLng(gpsLati, gpsLong);
 
                 // 마커를 생성합니다
-                marker = new window.kakao.maps.Marker({
+                let marker = new window.kakao.maps.Marker({
                   position: position,
                   clickable: true,
                 });
                 // 마커를 지도에 표시합니다.
                 marker.setMap(map);
-                
-
-              });
-              
-              window.kakao.maps.event.addListener(map, "click", function (mouseEvent:any) {
-                let latlng = mouseEvent.latLng;
-
-                position = new window.kakao.maps.LatLng(latlng.getLat(), latlng.getLng());
-                // 지도에 마커를 표시합니다 
-                var marker = new window.kakao.maps.Marker({
-                    map: map, 
-                    position: position
-                });
-
                 function BusChild(number: string, div: string, time: string, count: string) {
                   let colorVal;
                   let timeVal;
@@ -179,49 +158,6 @@ const MapWithMarkers: React.FC<MapProps> = () => {
                   }
                 };
                 
-                
-
-                if (toggle) {
-                  mark = markPull;
-                }
-                else {
-                  mark = markNull;
-                }
-
-                let content = `
-                <div class="busModalWin">
-                  <div class="titleWrap">
-                    <div class="titleArea">
-                      <p class="title">${busStopName}</p>
-                      <img src=${mark} alt="bookMark" onClick="${handleImageClick()}" />
-                      <div class="close" onclick="${closeOverlay()}" title="닫기"></div>
-                    </div>
-                    <div class="titleArea">
-                      <p class="busText">${busStopNumber}</p>
-                      <p class="busText">${busWay}</p>
-                    </div>
-                  </div>
-                  <div class="divLine"></div>
-                  <div class="busInfoWrap">
-                    <p class="busTitleWrap">실시간 버스 정보</p>
-                    <div class="listScroll">
-                      ${ BusList(cnt, busNumber, busDiv, busTime, busStopCount)}
-                    </div >
-                  </div >
-                </div > `;
-
-                let  customOverlay =  new window.kakao.maps.CustomOverlay({
-                  position: position,
-                  content: content,
-                  map: map
-                  // xAnchor: 0,
-                  // yAnchor: 0.3
-                  
-                });
-                
-                function closeOverlay() {
-                  customOverlay.setMap(null);     
-                }
                 // 마커에 클릭이벤트를 등록합니다
                 window.kakao.maps.event.addListener(marker, 'click', function () {
 
@@ -264,15 +200,53 @@ const MapWithMarkers: React.FC<MapProps> = () => {
                     responseHandlerRegistered = true;
                   }
 
+                  if (toggle) {
+                    mark = markPull;
+                  }
+                  else {
+                    mark = markNull;
+                  }
+  
+                  let content = `
+                    <div class="busModalWin">
+                      <div class="titleWrap">
+                        <div class="titleArea">
+                          <p class="title">${busStopName}</p>
+                          <img src=${mark} alt="bookMark" onClick="${handleImageClick()}" />
+                        </div>
+                        <div class="titleArea">
+                          <p class="busText">${busStopNumber}</p>
+                          <p class="busText">${busWay}</p>
+                        </div>
+                      </div>
+                      <div class="divLine"></div>
+                      <div class="busInfoWrap">
+                        <p class="busTitleWrap">실시간 버스 정보</p>
+                        <div class="listScroll">
+                          ${ BusList(cnt, busNumber, busDiv, busTime, busStopCount)}
+                        </div >
+                      </div >
+                    </div > `;
+  
+                  let customOverlay =  new window.kakao.maps.CustomOverlay({
+                    position: position,
+                    content: content,
+                    map: map
+                    // xAnchor: 0,
+                    // yAnchor: 0.3
+                    
+                  });
                   
 
                   customOverlay.setMap(map)
 
+                  setInterval(()=>{
+                    customOverlay.setMap(null)
+                  }, 5000)
+                  
                 });
-              });
-                
-                
 
+              });
               console.log(data);
             })
             .catch((error) => console.log(error));
